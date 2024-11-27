dd5dd6f99a4db95865cf4722869f506572dbce83
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-11-27 14:32:41 +0530

    Add Yargs

--- modules_node/index.js
+++ modules_node/index.js
@@ -1,13 +1,2 @@
 #!/usr/bin/env node
-
-import * as thing from "./utils";
-import fs from "node:fs";
-import _ from "lodash";
-// const { count } = require("./utils");
-
-const note = process.argv[2];
-const newNote = {
-  content: note,
-  id: Date.now(),
-};
-console.log(newNote);
+import "./src/command.js";
--- modules_node/utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const count = (num) => num;
-export const somethingElse = () => {};
-// exports.count = (num) => num;
-// module.exports = { count };
-export default { name: "cookies" };
